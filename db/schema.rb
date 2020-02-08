dae43cda5f0f1b0c79ddfffddd4e9713d264157e
t-otsuki1017 <59334182+t-otsuki1017@users.noreply.github.com>
2020-02-08 18:08:12 +0900

    Update schema.rb
    endを追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 2020_02_08_063552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_guests_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_guests_on_reset_password_token", unique: true
-
+    トン。インデックス [ "reset_password_token" ]、名前： "index_guests_on_reset_password_token"、一意： true
+　end
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "guest_id"
