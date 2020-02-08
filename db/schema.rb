e78c953f370b259296cd0c6fbc79bcb586356068
shibata yasunobu <shibayasu13@gmail.com>
2020-02-08 09:08:50 +0000

    更新完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_02_08_063552) do
 
   create_table "guests", force: :cascade do |t|
@@ -31,8 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_063552) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_guests_on_email", unique: true
     t.index ["reset_password_token"], name: "index_guests_on_reset_password_token", unique: true
-=======
-ActiveRecord::Schema.define(version: 2020_02_08_071108) do
+  end
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "guest_id"
@@ -90,7 +88,6 @@ ActiveRecord::Schema.define(version: 2020_02_08_071108) do
     t.integer "sales_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
->>>>>>> e95ab676e489ed519ba9b48b8e081d04418243bf
   end
 
 end
