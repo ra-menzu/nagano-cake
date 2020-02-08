69cbf1e2b0301be2cf74d724f479d2d0a401883a
hideki1104 <59091803+hideki1104@users.noreply.github.com>
2020-02-08 18:16:28 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_063552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_guests_on_email", unique: true
-    トン。インデックス [ "reset_password_token" ]、名前： "index_guests_on_reset_password_token"、一意： true
+    t.index [ "reset_password_token" ]、：name "index_guests_on_reset_password_token"、unique： true
 　end
 
   create_table "cart_items", force: :cascade do |t|
