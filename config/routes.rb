e40bfa29951ec21ee644440ca65fbe69160ecd19
shizuka <shizuka1119@gmail.com>
2020-02-14 09:00:59 +0000

    cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,24 @@ Rails.application.routes.draw do
   root "homes#top"
   get "home/about" => "homes#about"
 
-
   namespace :admin do
+    get'top' => 'admin#index'
     resources :genres, only: [:index, :edit, :update, :create]
     get 'orders/new'
-    get 'guests/new'
     resources :guests,only:[:index,:show,:edit,:update]
     resources :products
-    devise_scope :admin do 
-      get 'guests' => 'devise/sessions#new'
-      post 'guests' => 'devise/sessions#cteate'
-      delete 'guests' => 'devise/sessions#destroy'
-      get 'guests' => 'devise/registrations#new'
+    devise_scope :admin do
+      get 'guests/sign_in' => 'devise/sessions#new'
+      post 'guests/sign_in' => 'devise/sessions#cteate'
+      delete 'guests/sign_out' => 'devise/sessions#destroy'
+      get 'guests/sign_up' => 'devise/registrations#new'
       delete 'guests' => 'devise/registrations#destroy'
       post 'guests' => 'devise/registrations#create'
-    end 
+    end
   end
-
   devise_for :guests
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :guests,only:[:edit,:update,:show,:index,:destroy]
 
   namespace :guest do
   	resources :products
@@ -30,6 +29,6 @@ Rails.application.routes.draw do
     resources :delivery_addresses,only:[:index,:create,:edit,:update,:destroy]
   	resources :order_histories
   	resources :cart_items
+    delete 'cart_item' => 'cart_items#destroy_all'
   end
-   resources :guests,only:[:edit,:update,:show,:index,:destroy]
 end
