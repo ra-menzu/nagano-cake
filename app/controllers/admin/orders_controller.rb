81600d9d8ec6be3afe72e1ecff2b37a7da2be515
shizuka <shizuka1119@gmail.com>
2020-02-16 10:38:22 +0000

    admin

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,16 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @ordered_items = OrderedItem.all
+    @orders = Order.all
+    # @ordered_item = Order.find(params[:id])
+    # @sum = 0
+    # @total_quantity = []
+    # 一つの注文の合計を出す <= 10
+    # 配列に値を入れる total_quantity = [10]
+    # 2つめの注文の合計を出す <= 3
+    # 配列に値を入れる total_quantity = [10,3]
   end
   def show
      @ordered_item = OrderedItem.find(params[:id])
-     @order = Order.find(params[:id])
   end
   def update
     @order = Order.find(params[:id])
