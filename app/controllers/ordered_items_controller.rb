a8d59a624210ccbd01fdf57d45e87fee8265a6b2
shizuka <shizuka1119@gmail.com>
2020-02-17 05:11:45 +0000

    admin_order

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -1,8 +1,3 @@
-class OrderedItemsController < ApplicationController
-    def update
-    	@order = Order.find(params[:id])
-        @ordered_item = @order.ordered_items
-        @order.update(admin_order_params)
-        redirect_to admin_orders_path
-	end
+class OrderedItemsController < ApplicationControllerz
+
 end
