a03be1fbfef82deaf9cb53c8d63746a82c626016
shibata yasunobu <shibayasu13@gmail.com>
2020-02-17 05:40:27 +0000

    saisinn

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
 
-	  # before_action :authenticate_admin!
+	  before_action :authenticate_admin!
 
 
 	def top
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 	end
 
   private
-	def after_sign_in_path(resource)
+	def after_sign_in_path_for(resource)
 		case resource
 		when Admin
 			admin_top_path
