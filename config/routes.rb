7dae4c45299e0e4545c83c50325281d0bb429ddb
hideki1104 <hideki11047@gmail.com>
2020-02-17 05:58:53 +0000

    管理者のログイン後の画面設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+      sessions: 'admin/sessions'
+    }
   root "homes#top"
   get "home/about" => "homes#about"
   get'admin/top' => 'admin#top'
