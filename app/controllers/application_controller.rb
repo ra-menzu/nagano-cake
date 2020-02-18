735d0bb1eebdcae7513d1f2a69f3aca6bb543a2b
shibata yasunobu <shibayasu13@gmail.com>
2020-02-18 07:20:49 +0000

    saisinn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-		before_action :configure_permitted_parameters, if :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:last_name,:family_name_kana,:last_name_kana,:postcode,:street_adress,:phone_nember,:email])
 	end
 
-	private
-	    def after_sign_in_path_for(resource)
+  private
+	def after_sign_in_path_for(resource)
 		case resource
 		when Admin
 			admin_top_path
