17b39115a9b53f9319bfb61614252eca67df6358
shizuka <shizuka1119@gmail.com>
2020-02-18 07:25:26 +0000

    admin

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,3 +1,8 @@
 class DeliveryAddress < ApplicationRecord
+	validates :postal_code, presence: true
+	validates :postal_adress, presence: true
+	validates :destination, presence: true
 	belongs_to :guest
 end
+
+
